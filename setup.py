1d15a913b8136a6546f731fbccd6245d2097a7ac
Joway Wang <joway.w@gmail.com>
2018-01-01 10:37:19 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pylemon',
+    version='0.0.0',
+    description='An async and lightweight restful framework for python .',
+    url='https://github.com/joway/lemon',
+    author='Joway Wang',
+    author_email='joway.w@gmail.com',
+    license='MIT',
+    packages=['lemon'],
+    keywords='lemon restful api async python',
+)
