eee1c6a86e8d1cd4c2663109282e25ce100df3dd
Joway Wang <joway.w@gmail.com>
2018-01-30 10:55:50 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
-DEVELOPMENT_STATUS = "3 - Alpha"
+PACKAGE_VERSION = '0.0.8'
+PACKAGE_REQUIRES = [
+    'uvicorn>=0.0.15',
+    'kua>=0.2',
+    'werkzeug>=0.14.1',
+]
+DEVELOPMENT_STATUS = '3 - Alpha'
 
 setup(
     name='pylemon',
-    version='0.0.8',
+    version=PACKAGE_VERSION,
     description='An async and lightweight API framework for python .',
     url='https://github.com/joway/lemon',
     author='Joway Wang',
@@ -14,22 +20,18 @@ setup(
     keywords='lemon restful api async python',
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    install_requires=[
-        'uvicorn>=0.0.15',
-        'kua>=0.2',
-        'werkzeug>=0.14.1',
-    ],
+    install_requires=PACKAGE_REQUIRES,
     classifiers=[
-        "Development Status :: %s" % DEVELOPMENT_STATUS,
-        "Framework :: AsyncIO",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        'Development Status :: %s' % DEVELOPMENT_STATUS,
+        'Framework :: AsyncIO',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
